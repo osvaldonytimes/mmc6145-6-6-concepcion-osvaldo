5af95b336257da8080f9a39c296858d02a922dff
Osvaldo Concepcion <osvaldoconcep@gmail.com>
2023-11-01 19:54:36 -0400

    auth actions initial implementation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a52c8b541f7d9aac"; }
+/******/ 		__webpack_require__.h = function() { return "1bdde12c3150e6e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/auth/[action].js
+++ pages/api/auth/[action].js
@@ -1,51 +1,61 @@
 import { withIronSessionApiRoute } from "iron-session/next";
-import sessionOptions from "../../../config/session"
-import db from '../../../db'
+import sessionOptions from "../../../config/session";
+import db from "../../../db";
 
 // this file handles /api/auth/:action with any request method (GET, POST, etc)
-export default withIronSessionApiRoute(
-  function handler(req, res) {
-    // console.log(req.query.action)
-    // console.log(req.method)
-    // TODO: implement POST /api/auth/login
-    // TODO: implement POST /api/auth/logout
-    // TODO: implement POST /api/auth/signup
-    return res.status(404).end()
-  },
-  sessionOptions
-)
+export default withIronSessionApiRoute(async function handler(req, res) {
+  const action = req.query.action;
+  if (req.method === "POST") {
+    switch (action) {
+      case "login":
+        console.log("login call");
+        await login(req, res);
+        break;
+      case "logout":
+        await logout(req, res);
+        break;
+      case "signup":
+        await signup(req, res);
+        break;
+      default:
+        res.status(404).end();
+    }
+  } else {
+    res.status(405).end();
+  }
+}, sessionOptions);
 
 async function login(req, res) {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   try {
-    const user = await db.auth.login(username, password)
+    const user = await db.auth.login(username, password);
     req.session.user = {
       username: user.username,
-      id: user.id
-    }
-    await req.session.save()
-    res.status(200).end()
-  } catch(err) {
-    res.status(400).json({error: err.message})
+      id: user.id,
+    };
+    await req.session.save();
+    res.status(200).end();
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 }
 
 async function logout(req, res) {
-  await req.session.destroy()
-  res.status(200).end()
+  await req.session.destroy();
+  res.status(200).end();
 }
 
 async function signup(req, res) {
   try {
-    const {username, password} = req.body
-    const user = await db.user.create(username, password)
+    const { username, password } = req.body;
+    const user = await db.user.create(username, password);
     req.session.user = {
       username: user.username,
-      id: user.id
-    }
-    await req.session.save()
-    res.redirect('/search')
-  } catch(err) {
-    res.status(400).json({error: err.message})
+      id: user.id,
+    };
+    await req.session.save();
+    res.redirect("/search");
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 }
